ca2551d0054dafc067d8d04650cd7bafe7fd94dc
martin.deguzman <martindeguzman16@gmail.com>
2017-05-06 13:37:58 +0800

    fixed responsiveness issue on team

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,1 +1,45 @@
 /* Write here your custom javascript codes */
+
+
+/*$(window).resize(function(){
+
+	var h_height = $("#quote-h").height();
+
+	//$("#qoute-m").height(h_height + "px");
+	
+
+});*/
+var Custom = new function(){
+	
+	var self = this;
+	
+	
+	//methods
+	this.initComponents = function(){
+		self.initProperties();
+		self.initEvents();
+		
+	}
+	
+	this.initProperties = function(){
+	
+	}
+	
+	this.initEvents = function(){
+
+		$(window).resize(self.resizeTeam);
+
+	}
+	
+
+	//util methods
+	this.resizeTeam = function(){
+		var h_height = $("#quote-harvey").height();
+		$("#quote-martin").height(h_height + 30);
+
+	}
+
+}
+
+Custom.initComponents();
+Custom.resizeTeam();
